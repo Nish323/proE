3bee2bc5c50198ce9f14136248b4b1d0013aed2a
Nish323 <doragon.nini@icloud.com>
2026-04-08 02:19:31 +0900

    add Navbarのコンポーネントの追加

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,18 +1,50 @@
 import React from "react";
 import "./Navbar.css";
+import axios, { AxiosError } from "axios";
+import { useState, useEffect } from "react";
 import { RefreshCcw } from "lucide-react";
 
+type PredictionData = {
+  prediction: string;
+  timestamp: string;
+};
+
 export function Navbar() {
-	return (
-		<nav className="navbar">
-			<div className="navbar-logo">
-				<div className="navbar-main-logo">PALTO-AI</div>
-				<div className="navbar-sub-logo">crowedview</div>
-			</div>
-			<button className="navbar-button">
-				<RefreshCcw />
-				<p>Update</p>
-			</button>
-		</nav>
-	);
+  const [data, setData] = useState<PredictionData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+	const [refresh, setRefresh] = useState<boolean>(false);
+
+  const fetchData = () => {
+    axios
+      .get<PredictionData>("http://localhost:5001/prediction")
+      .then((res) => {
+        setData(res.data);
+        setError(null);
+      })
+      .catch((err: AxiosError) => {
+        console.error("Error fetching data:", err);
+        setError(`データの取得に失敗しました: ${err.message}`);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [refresh]);
+
+  const handleRefreshClick = () => {
+    setRefresh((prev) => !prev);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <div className="navbar-main-logo">PALTO-AI</div>
+        <div className="navbar-sub-logo">crowd view</div>
+      </div>
+      <button onClick={handleRefreshClick} className="navbar-button">
+        <RefreshCcw className="navbar-button-icon" />
+        <p className="navbar-button-text">Update</p>
+      </button>
+    </nav>
+  );
 }
